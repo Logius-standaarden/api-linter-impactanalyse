8671d7cd42e08f28bc299218139c2a5156c18543
Tim van der Lippe <Tim.Lippe@logius.nl>
2025-10-28 15:26:52 +0100

    Gebruik headers van API om einde te bepalen
    Naar aanleiding van de suggestie in
    https://github.com/developer-overheid-nl/don-api-register/issues/32#issuecomment-3430921325

--- scripts/download-api-register.py
+++ scripts/download-api-register.py
@@ -15,18 +15,28 @@ API_DEFINITIONS_DIRECTORY = API_REGISTER_DIRECTORY / "definitions"
 
 API_REGISTER_API_ENDPOINT = "https://api.don.apps.digilab.network/api-register/v1/apis"
 API_REGISTER_HEADERS = {'x-api-key': os.environ['API_KEY']}
-page = 0
+# Pages are one-indexed in the register
+page = 1
+# This is a dummy value to ensure we run the loop at least once. After the
+# first request, this set to the correct amount
+total_pages = 2
 apis_in_api_register = []
-while True:
+# This is a dummy value. After the first request, this set to the correct amount
+total_apis = -1
+while page < total_pages + 1:
     try:
         apis_in_page = requests.get(API_REGISTER_API_ENDPOINT, verify=False, headers=API_REGISTER_HEADERS, params={'perPage': '50', 'page': page})
         page += 1
         apis = apis_in_page.json()
-        if len(apis) == 0:
-            break
         apis_in_api_register.extend(apis)
+        total_pages = int(apis_in_page.headers["x-total-pages"])
+        total_apis = int(apis_in_page.headers["x-total-count"])
     except urllib.error.HTTPError as http_error:
         break
+
+if len(apis_in_api_register) != total_apis:
+    print(f"Invalid number of api's retrieved. Register reported {total_apis}, but retrieved {len(apis_in_api_register)}")
+    sys.exit(1)
 
 API_DEFINITIONS_DIRECTORY.mkdir(parents=True, exist_ok=True)
 for filename in os.listdir(API_DEFINITIONS_DIRECTORY):
